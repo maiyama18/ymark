93a9c18678d039adf36e49549718ec8e94b774c5
ymr-39 <myam0118@gmail.com>
2018-12-17 09:56:06 +0900

    add constructors to nodes

--- src/node/node.ts
+++ src/node/node.ts
@@ -1,31 +1,47 @@
-import {Token} from '../token/token';
-
 export type Node = Line | Inline;
 
 // Line nodes
 export type Line = Paragraph | Header;
 
 export class Paragraph {
-  public nodeType: 'PARAGRAPH';
+  public readonly nodeType = 'PARAGRAPH';
   public inlines: Inline[];
+
+  constructor(inlines?: Inline[]) {
+    this.inlines = inlines || [];
+  }
 }
 
 export class Header {
-  public nodeType: 'HEADER';
-  public inlines: Inline[];
+  public readonly nodeType = 'HEADER';
   public numHashes: number;
+  public inlines: Inline[];
+
+  constructor(numHashes: number, inlines?: Inline[]) {
+    this.numHashes = numHashes;
+    this.inlines = inlines || [];
+  }
 }
 
 // Inline nodes
 export type Inline = Text | Link;
 
 export class Text {
-  public nodeType: 'TEXT';
+  public readonly nodeType = 'TEXT';
   public text: string;
+
+  constructor(text?: string) {
+    this.text = text || '';
+  }
 }
 
 export class Link {
-  public nodeType: 'LINK';
+  public readonly nodeType = 'LINK';
   public text: string;
   public href: string;
+
+  constructor(text?: string, href?: string) {
+    this.text = text || '';
+    this.href = href || '';
+  }
 }
